7978b11ff24eed0bbe2b1674118af137027bd43c
YuriMelloFile <yurimello29@gmail.com>
2022-04-22 23:31:46 -0300

    Sexto commit

--- desafio_3.js
+++ desafio_3.js
@@ -7,98 +7,155 @@
         • Transformar a função acima em uma função que irá receber o nome do autor e devolver os livros desse autor.
 */
 
-const booksByCategory = [
-    {
-        category: "Riqueza",
-        books: [
-            {
-                title: "Os segredos da mente milionária",
-                author: "T. Harv Eker",
-            },
-            {
-                title: "O homem mais rico da Babilônia",
-                author: "George S. Clason",
-            },
-            {
-                title: "Pai rico, pai pobre",
-                author: "Robert T. Kiyosaki e Sharon L. Lechter",
-            },
-        ],
-    },
-    {
-        category: "Inteligência Emocional",
-        books: [
-            {
-                title: "Você é Insubstituível",
-                author: "Augusto Cury",
-            },
-            {
-                title: "Ansiedade – Como enfrentar o mal do século",
-                author: "Augusto Cury",
-            },
-            {
-                title: "Os 7 hábitos das pessoas altamente eficazes",
-                author: "Stephen R. Covey",
-            },
-            {
-                title: "Pai rico pai pobre ",
-                author: "Robert Kiyosaki",
-            },
-        ],
-    },
-];
 
+function desafio_3(){
 
-/* Contando o número de categorias e o número de livros em cada categoria*/
-
-let Allcategories = 0
-let books = 0
-
-for (let content of booksByCategory){
+    const booksByCategory = [
+        {
+            category: "Riqueza",
+            books: [
+                {
+                    title: "Os segredos da mente milionária",
+                    author: "T. Harv Eker",
+                },
+                {
+                    title: "O homem mais rico da Babilônia",
+                    author: "George S. Clason",
+                },
+                {
+                    title: "Pai rico, pai pobre",
+                    author: "Robert T. Kiyosaki e Sharon L. Lechter",
+                },
+            ],
+        },
+        {
+            category: "Inteligência Emocional",
+            books: [
+                {
+                    title: "Você é Insubstituível",
+                    author: "Augusto Cury",
+                },
+                {
+                    title: "Ansiedade – Como enfrentar o mal do século",
+                    author: "Augusto Cury",
+                },
+                {
+                    title: "Os 7 hábitos das pessoas altamente eficazes",
+                    author: "Stephen R. Covey",
+                },
+                {
+                    title: "Jogada perigosa ",
+                    author: "Robert Kiyosaki",
+                },
+            ],
+        },
+    ];
     
-    if (content.category != null){
-        Allcategories += 1
-
-    } 
     
-    if (content.books != null){
+    /* Contando o número de categorias e o número de livros em cada categoria*/
+    
+    let Allcategories = 0
+    let books = 0
+    
+    for (let content of booksByCategory){
         
-        for (authorsAndTitle of content.books){
-            books +=1
+        if (content.category != null){
+            Allcategories += 1
+    
+        } 
+        
+        if (content.books != null){
             
-
+            for (authorsAndTitle of content.books){
+                books +=1
+                
+    
+            }
+            
+            
+    
         }
         
         
-
+        
+        console.log(`O total de livros na ${Allcategories}° categoria : ' ${content.category} ',  é : ${books}`)  
+        books= 0
     }
     
     
     
-    console.log(`O total de livros na ${Allcategories}° categoria : ' ${content.category} ',  é : ${books}`)  
-    books= 0
-}
-
-
-
-/*  Contar o número de autores */
-
-
-let authors = 0
-for (let content of booksByCategory){
-        
-    if (content.books != null){
-        
-        for (authorsAndTitle of content.books){
-            
-            let cont = authorsAndTitle.author != null ? authors+=1 : pass; 
-            
-
-        }
-               
-
-    }       
+    /*  Contar o número de autores */
     
+    
+    let authors = 0
+    for (let content of booksByCategory){
+            
+        if (content.books != null){
+            
+            for (authorsAndTitle of content.books){
+                
+                let cont = authorsAndTitle.author != null ? authors+=1 : pass; 
+                
+    
+            }
+                   
+    
+        }       
+        
+    }
+    
+    console.log(`O total de autores é : ${authors}.`)
+    
+    
+    /* Mostrar livros do autor Auguto Cury */ 
+    
+    
+    for (let content of booksByCategory){
+            
+        if (content.books != null){
+            
+            for (authorsAndTitle of content.books){
+                
+                if (authorsAndTitle.author == "Augusto Cury"){
+                    
+                    console.log(`Este é um livro de Augusto Cury : ${authorsAndTitle.title} `)
+                }  
+                
+            }             
+    
+        }       
+        
+    }
+    
+    
+    /*Transformar a função acima em uma função que irá receber o nome do autor e devolver os livros desse autor.*/
+    
+    /*
+    let answer = prompt("Digite o nome de um autor  : ")
+    
+    for (let content of booksByCategory){
+            
+        if (content.books != null){
+            
+            for (authorsAndTitle of content.books){
+                
+                if (authorsAndTitle.author == answer){
+                    
+                    console.log(`Este é um livro de ${answer} : ${authorsAndTitle.title} `)
+                }  
+                
+            }             
+    
+        }       
+        
+    }
+    
+    */
+
+
+
+
 }
 
-console.log(`O total de autores é : ${authors}.`)
+
+
